e357bc8d42467d7c821a97416f76a85682fb3c38
be <be.han.eps@gmail.com>
2024-07-19 00:36:42 -0700

    Update schema.sql

--- Pewlett-Hackard/data/schema.sql
+++ Pewlett-Hackard/data/schema.sql
@@ -2,8 +2,8 @@ DROP TABLE employees IF EXISTS;
 DROP TABLE titles IF EXISTS;
 DROP TABLE salaries IF EXISTS;
 DROP TABLE departments IF EXISTS;
-DROP TABLE dept_employees IF EXISTS;
-DROP TABLE dept_managers IF EXISTS;
+DROP TABLE dept_emp IF EXISTS;
+DROP TABLE dept_mng IF EXISTS;
 
 CREATE TABLE employees(
     id INT,
@@ -13,29 +13,35 @@ CREATE TABLE employees(
     last_name VARCHAR(16),
     sex VARCHAR(1),
     hire_date VARCHAR(10)
-);
+); 
+SELECT * FROM employees;
 
 CREATE TABLE titles(
     title_id VARCHAR(5),
     title VARCHAR(18)
 );
+SELECT * FROM titles;
 
 CREATE TABLE salaries(
     id INT,
     salary INT
 );
+SELECT * FROM salaries;
 
 CREATE TABLE departments(
     dept_id VARCHAR(4),
     dept_name VARCHAR(18)
 );
+SELECT * FROM departments;
 
 CREATE TABLE dept_emp(
     id INT,
-    dept_id INT
+    dept_id VARCHAR(4)
 );
+SELECT * FROM dept_emp;
 
 CREATE TABLE dept_mng(
     dept_id VARCHAR(4),
     id INT
 );
+SELECT * FROM dept_mng;
